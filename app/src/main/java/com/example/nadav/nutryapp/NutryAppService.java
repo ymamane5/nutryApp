6f36afc331a17fd73d1412dec0b7d7970c9cd838
ymamane5 <ymamane5@gmail.com>
2019-03-13 23:06:08 +0200

    update

--- app/src/main/java/com/example/nadav/nutryapp/NutryAppService.java
+++ app/src/main/java/com/example/nadav/nutryapp/NutryAppService.java
@@ -1,13 +1,9 @@
 package com.example.nadav.nutryapp;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.provider.Settings;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -28,7 +24,7 @@ public class NutryAppService extends Service {
         // showNotification(intent.getStringExtra("KEY1"));
         //Log.v("reminder_tag", "inside service onStartCommand");
 
-        player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        player = MediaPlayer.create(this, R.raw.backgroundmusic);
         player.setLooping(true);
         player.start();
 
@@ -45,18 +41,6 @@ public class NutryAppService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
-    }
-
-     public void showNotification(String str) {
-        String notificationTag = "notificationTag2";
-        int notificationId = 234;
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext(), "1")
-                .setSmallIcon(R.drawable.image)
-                .setContentTitle("NutrApp notification")
-                .setContentText("str")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        NotificationManagerCompat.from(getBaseContext()).notify(notificationTag, notificationId, builder.build());
     }
 
 }
